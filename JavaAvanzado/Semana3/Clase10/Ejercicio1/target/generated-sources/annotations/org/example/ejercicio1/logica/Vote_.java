6f640260b7717912e51cd42a866c8037be7de919
David Fernández <davidfernandezcomesana@gmail.com>
2024-01-11 19:32:22 +0100

    ej clase10

---
+++ JavaAvanzado/Semana3/Clase10/Ejercicio1/target/generated-sources/annotations/org/example/ejercicio1/logica/Vote_.java
@@ -0,0 +1,14 @@
+package org.example.ejercicio1.logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2024-01-11T16:37:56")
+@StaticMetamodel(Vote.class)
+public class Vote_ { 
+
+    public static volatile SingularAttribute<Vote, String> name;
+    public static volatile SingularAttribute<Vote, Long> id;
+
+}
